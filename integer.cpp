b8e616c3066352ebb19d3264df94cd43aee79121
Lukas <schmid.lukas97@yahoo.de>
2021-11-03 02:02:39 +0100

    changes

--- integer.cpp
+++ integer.cpp
@@ -40,7 +40,7 @@ extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
-	int Integer_fib(Integer* integer, int n) {return integer->fib(n);}
+	int Integer_fib(Integer* integer, int n) {return integer->fib();}
 	void Integer_delete(Integer* integer){
 		if (integer){
 			delete integer;
